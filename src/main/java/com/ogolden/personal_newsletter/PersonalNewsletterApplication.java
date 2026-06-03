02dddccc31b1ef1289c85c93f93e1765b1654eac
oegolden <ogolden@g.hmc.edu>
2026-06-02 20:57:04 -0700

    General API structure and framework

---
+++ src/main/java/com/ogolden/personal_newsletter/PersonalNewsletterApplication.java
@@ -0,0 +1,12 @@
+package com.ogolden.personal_newsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalNewsletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalNewsletterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ogolden/personal_newsletter/PersonalNewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogolden.personal_newsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalNewsletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
